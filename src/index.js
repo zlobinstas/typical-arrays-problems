67238a39be934cad6c434eeb2c448389a7a38f71
zlobinstas <zlobinstas@bk.ru>
2021-02-23 21:46:29 +0100

    init: start typical-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    return (!array || array.length == 0) ? 0 : array.reduce((a,b) => Math.min(a,b));
+};
 
 exports.max = function max (array) {
-  return 0;
+  return (!array || array.length == 0) ? 0 : array.reduce((a,b) => Math.max(a,b));
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (!array || array.length == 0) ? 0 : array.reduce((a, b) => (a + b)) / array.length;
 }
+
+
